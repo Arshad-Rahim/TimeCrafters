ed91b58fb859b6b6e32744e133034d5e4f916a9c
Arshad-Rahim <rahimarshad667@gmail.com>
2024-09-27 22:33:11 +0530

    view engine setup

---
+++ controllers/user/userController.js
@@ -0,0 +1,16 @@
+
+
+
+const loadHome = async (req,res)=>{
+    try{
+        return res.render('home');
+    }
+    catch(error){
+        console.log("Home page  not found");
+        res.status(500).send('Server error')
+    }
+}
+
+module.exports={
+    loadHome,
+}
--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const env = require('dotenv').config();
-const db = require('../first project/config/db');
+const env = require("dotenv").config();
+const path = require("path");
+const db = require("../first project/config/db");
 db();
+const userRouter = require("./routes/userRouter");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000, ()=>console.log("Server is running"))
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.set("views", [
+  path.join(__dirname, "views/user"),
+  path.join(__dirname, "views/admin"),
+]);
+
+app.use("/", userRouter);
+
+app.listen(3000, () => console.log("Server is running"));
